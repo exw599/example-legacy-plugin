730483703cb7848da5da7fb06eef8851c9194d9f
exw599 <c.gu@qmul.ac.uk>
2018-10-29 16:53:09 +0000

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/main/java/com/mycompany/imagej/SedTrack.java
+++ src/main/java/com/mycompany/imagej/SedTrack.java
@@ -21,7 +21,7 @@ import ij.process.ImageProcessor;
  *
  * @author Johannes Schindelin
  */
-public class Process_Pixels implements PlugInFilter {
+public class SedTrack implements PlugInFilter {
 	protected ImagePlus image;
 
 	// image property members
@@ -170,7 +170,7 @@ public class Process_Pixels implements PlugInFilter {
 	 */
 	public static void main(String[] args) {
 		// set the plugins.dir property to make the plugin appear in the Plugins menu
-		Class<?> clazz = Process_Pixels.class;
+		Class<?> clazz = SedTrack.class;
 		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
 		String pluginsDir = url.substring("file:".length(), url.length() - clazz.getName().length() - ".class".length());
 		System.setProperty("plugins.dir", pluginsDir);
